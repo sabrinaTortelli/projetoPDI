0b8a154c16535bbc1e0e6420ed931c7296c39d10
Ricardo Heffel <heffel13@gmail.com>
2023-03-20 02:31:56 -0300

    adding histogram

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from image_io.image_processor import ImageProcessor as IP
 from value.gray_scale import GrayScale as GS
 from value.rgb_channels import Extractor as EX
+from value.histogram import Histogram as Hs
 
 
 if __name__ == '__main__':
     img = IP("joker.jpg")
+    #img = IP("dog.jpg")
     #img.show_image("Teste")
     #img.save_image("Test014")
     #gray_img = GS(img.return_image_object())
     #gray_img.gray_convert_simple()
     #gray_img.gray_convert_ponder()
-    chan_img = EX(img.return_image_object())
-    chan_img.blue_channel()
+    #chan_img = EX(img.return_image_object())
+    #chan_img.blue_channel()
+    Hs(img.return_image_object()).plot_hist()
+    print("YO")
 
 
 
--- value/gray_scale.py
+++ value/gray_scale.py
@@ -1,8 +1,8 @@
 import cv2
 
 
-
 class GrayPonder:
+    # recebe um pixel a multiplica os valores RGB para calcular a média ponderada
     def gray_pixel_manipulation(pixel):
         pixel[2] = pixel[2] * 0.1140  # blue
         pixel[1] = pixel[1] * 0.5870  # green
@@ -11,12 +11,16 @@ class GrayPonder:
 
 
 class GrayScale(object):
+    # recebe um objeto imagem e armazena o numero de colunas e linhas da matriz imagem
     def __init__(self, image):
         self.image = image
         self.row = self.image.shape[0]
         self.col = self.image.shape[1]
 
     def gray_convert_simple(self):
+        #recebe uma matriz imagem e percorre todas as linhas e colunas
+        #somando os pixels RGB de cada posicão e faz a média simples
+        #deixando a imgem com tons de cinza
         for i in range(self.row):
             for j in range(self.col):
                 self.image[i, j] = sum(self.image[i, j]) * 0.33
@@ -25,6 +29,9 @@ class GrayScale(object):
         return self.image
 
     def gray_convert_ponder(self):
+        # recebe uma matriz imagem e percorre todas as linhas e colunas
+        # chamando o metodo gray_manipulation para realizar a média ponderada dos pixels
+        # deixando a imgem com tons de cinza
         for i in range(self.row):
             for j in range(self.col):
                 self.image[i, j] = sum(GrayPonder.gray_pixel_manipulation(self.image[i, j]))
