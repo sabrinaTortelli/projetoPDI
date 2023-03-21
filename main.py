c2b24bef78b1b28ce576c33962c113b3c3a17114
Heffel <heffel13@gmail.com>
2023-03-20 21:59:06 -0300

    adding black/white threshold

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from value.histogram import Histogram as Hs
 
 
 if __name__ == '__main__':
-    img = IP("dog.jpg")
+    img = IP("joker.jpg")
     #img = IP("dog.jpg")
     #img.show_image("Teste")
     #img.save_image("Test014")
-    #gray_img = GS(img.return_image_object())
+    gray_img = GS(img.return_image_object())
     #gray_img.gray_convert_simple()
     #gray_img.gray_convert_ponder()
+
+
     #chan_img = EX(img.return_image_object())
     #chan_img.blue_channel()
-    Hs(img.return_image_object()).plot_hist()
+    #Hs(img.return_image_object()).plot_hist()
+    gray_img.threshold(200)
     print("YO")
 
 
